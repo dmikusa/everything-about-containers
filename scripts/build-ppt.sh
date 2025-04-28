957f410eaf4848b60b4d073c9e8cf7e3134fdebb
Daniel Mikusa <dan@mikusa.com>
2025-04-27 23:49:18 -0400

    Build pdf & pptx
    Signed-off-by: Daniel Mikusa <dan@mikusa.com>

---
+++ scripts/build-ppt.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -eo pipefail
+
+if [ ! -d "./slides" ]; then
+    echo "Error: slides directory not found. Please run this script from the root of the repository."
+    exit 1
+fi
+
+rm -rf ./out/
+mkdir ./out/
+marp -o ./out/ \
+    -I ./slides \
+    --no-config-file \
+    --pptx \
+    --browser chrome \
+    --browser-path /Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser \
+    --title "Everything you wanted to know about containers but were afraid to ask" \
+    --author "Daniel Mikusa" \
+    --keywords "containers, docker, kubernetes" \
+    --url "https://github.com/dmikusa/everything-about-containers"
